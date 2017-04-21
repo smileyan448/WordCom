00a3352c4f75f01bbd90371ec85cd1a79d4a54eb
smileyan448 <cyjia@bjtu.edu.cn>
2017-04-21 11:27:14 +0800

    Add files via upload

---
+++ ProPrecess/PrePro_C_code/getwordlist.cpp
@@ -0,0 +1,139 @@
+#include "getwordlist.h"
+extern char word[MAXLEN];
+int getword(int limit, char w[], FILE *fp)
+ {
+	int c, i = 0;
+	while(i<limit-2 && (c=fgetc(fp))!=EOF && c!='\n' && c!=' ' && c !='\t')
+ 	{
+		word[i] = c;  ++i;
+	} 
+	word[i] = '\0';   
+	return i;
+}
+
+LINK mknode(char w[])  //创建一个新结点
+{	LINK p = (LINK)malloc(sizeof(NODE));
+	if(p != NULL)
+	{	strncpy(p->word, w, MAXLEN);
+		p->count = 1;
+		p->next = NULL;
+	}
+	return p;
+}
+
+LINK addword(LINK list, char w[]) 
+//修改结点计数器或将新创建的结点加下链表中
+{	LINK p;
+	if (list == NULL) return mknode(w);
+	for(p=list; ; p=p->next)
+	{	if(strcmp(p->word,w)==0)
+		{	p->count++;
+			break;
+		}
+		if(p->next == NULL)
+		{	p->next = mknode(w);
+			break;
+		}
+	}
+	return list;
+}
+
+LINK deletword(LINK list, int highest) 
+//修改结点计数器或将新创建的结点加下链表中
+{	LINK p, q;
+	if (list == NULL) return list;
+	for(q=p=list; ; p=p->next)
+	{
+		if(p->count > highest)
+		{	
+			printf("%s\t%d\n", p->word, p->count);
+		//	q->next=p->next;
+		//	free(q);
+		}
+		q=p;	
+		if(p->next == NULL)
+		{
+			break;
+		}
+	
+	}
+	return list;
+}
+
+
+
+void printwords(LINK p, char filename[])    //遍历打印链表结点
+{
+	int count = 0;
+	FILE *fp;
+
+	if ((fp=fopen(filename,"w"))==NULL)
+	{	printf("1:cann't open file");
+      	exit(0);
+	}
+
+	for(; p!=NULL; p=p->next)
+	{
+		fprintf(fp, "%d %s\n", p->count, p->word);
+		count++;	
+	}
+
+	printf("total words: %d\n", count);
+
+	fclose(fp);
+}
+
+
+void docwordmatirx(LINK list, FILE *ifp, char * docwordfile)
+{
+	char string[MAXLINE];
+	FILE *ofp;
+	LINK p;
+	
+	if ((ofp=fopen(docwordfile,"w"))==NULL)
+    {	printf("cann't open file");
+      	exit(2);
+    }
+	while (fgets(string, MAXLINE, ifp) != NULL)
+	{
+			for(p=list; p!=NULL; p=p->next)
+			{
+				if(p->next != NULL)
+				{
+					if(strstr(string, p->word))
+						fprintf(ofp, "%d\t", 1);
+					else
+						fprintf(ofp, "%d\t", 0);
+				}
+				else
+				{
+					if(strstr(string, p->word))
+						fprintf(ofp, "%d\n", 1);
+					else
+						fprintf(ofp, "%d\n", 0);
+				}
+		
+			}
+				
+	}
+	fclose(ofp);
+}
+
+int wordCount(char *string) 
+{
+   int c = ' ', count = 0, i=0;
+   while (c != '\n') 
+	{
+      while ((c=string[i]) != '\n' && 
+             (c==' ' || c=='\t'))
+          i++;
+      if (c == '\n') break;
+      ++count;
+      while ((c=string[i]) != '\n' && 
+           c!=' ' && c!='\t')
+          i++;
+   }
+ //  printf("word count: %d\n", count);
+   return count;
+}
+
